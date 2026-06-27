e8ca969971ca4892286e91e4bfe0371d364419e6
Xephyrose <99614988+Xephyrose@users.noreply.github.com>
2026-06-27 16:44:40 -0400

    move pointlight3dgpu to /src/

--- src/PointLight3DGPU.h
+++ src/PointLight3DGPU.h
@@ -1,6 +1,6 @@
 #ifndef FIGMENTENGINE_POINTLIGHT3DGPU_H
 #define FIGMENTENGINE_POINTLIGHT3DGPU_H
-#include "thirdparty/glm/vec4.hpp"
+#include "../thirdparty/glm/vec4.hpp"
 
 struct PointLight3DGPU {
     glm::vec4 color = glm::vec4(1.0f); // xyz is color, w is intensity
